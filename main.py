73fca087f5a8ff3dfd8859af579092d330bb7baa
shivamramanuj <shivam.ramanuj85111@gmail.com>
2025-12-30 22:27:15 +0530

    Day2: add pratie message from the feature branch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 print("Hello Git")
 print("Working on feature branch")
 
-print("Daily Git Pratice - Day 1")
+print("Daily Git Practice - Day 1")
 
 print("This line is from the feature branch")
+
+print("Day 2 git Practice feature")
